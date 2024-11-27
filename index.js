e2172190ee697d0546dae842f75072af72c49c55
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-11-26 21:01:41 -0700

    solution

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+require('datejs');
 
+function combineUsers(...args){
+  const combineObject = {
+    users: []
+  }
+  args.forEach((usernames)=>{
+    combineObject.users = [...combineObject.users,...usernames]
+  })
+  
+  combineObject.merge_date = Date.today().toString("M/d/yyyy") 
+
+  return combineObject
+}
 
 
 module.exports = {
